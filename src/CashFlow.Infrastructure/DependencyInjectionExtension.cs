e780d0a0e0deeda09e45ddb143ce4de1f479b825
Douglas dos Santos Oliveira <doug1306@gmail.com>
2025-09-19 21:46:07 -0300

    feat: add interfaces

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,7 @@
 ﻿using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Repositories.User;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +16,8 @@ public static class DependencyInjectionExtension
     {
         AddDbContext(services, configuration);
         AddRepositories(services);
+
+        services.AddScoped<IPasswordEncripter, Infrastructure.Security.BCrypt>();
     }
 
     private static void AddRepositories(IServiceCollection services)
@@ -22,6 +26,8 @@ public static class DependencyInjectionExtension
         services.AddScoped<IExpensesReadonlyRepository, ExpensesRepository>();
         services.AddScoped<IExpensesWriteOnlyRepository, ExpensesRepository>();
         services.AddScoped<IExpensesUpdateOnlyRepository, ExpensesRepository>();
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
