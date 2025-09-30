ea4f2f19d5d14d2edd6d3350025fd3bbaa1c3475
Douglas dos Santos Oliveira <doug1306@gmail.com>
2025-09-29 22:18:16 -0300

    feat: enhance CustomWebApplicationFactory to generate access tokens and add RegisterExpensesTest for expense registration validation

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
   private CashFlow.Domain.Entities.User _user = null!;
   private string _password = null!;
+  private string _token = string.Empty;
   protected override void ConfigureWebHost(IWebHostBuilder builder)
   {
     builder.UseEnvironment("Test")
@@ -29,14 +31,18 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         
         var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
         var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+        var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
 
         StartDatabase(dbContext, passwordEncripter);
+
+        _token = tokenGenerator.Generate(_user);
       });
   }
 
   public string GetEmail() => _user.Email; 
   public string GetName() => _user.Name;
   public string GetPassword() => _password;
+  public string GetToken() => _token;
   
   private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
   {
---
+++ tests/WebApi.Test/Expenses/Register/RegisterExpensesTest.cs
@@ -0,0 +1,69 @@
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CashFlow.Exception;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.Timeouts;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Expenses.Register;
+
+public class RegisterExpensesTest: IClassFixture<CustomWebApplicationFactory>
+{
+  private const string METHOD = "api/Expenses";
+  private readonly HttpClient _httpClient;
+  private readonly string _token;
+
+  public RegisterExpensesTest(CustomWebApplicationFactory webApplicationFactory)
+  {
+    _httpClient = webApplicationFactory.CreateClient();
+    _token = webApplicationFactory.GetToken();
+  }
+
+  [Fact]
+  public async Task Success()
+  {
+    var request = RequestExpenseJsonBuilder.Build();
+    
+    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+    
+    var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+    result.StatusCode.Should().Be(HttpStatusCode.Created);
+    
+    var body = await result.Content.ReadAsStreamAsync();
+
+    var response = await JsonDocument.ParseAsync(body);
+    response.RootElement.GetProperty("title").GetString().Should().Be(request.Title);
+  }
+
+  [Theory]
+  [ClassData(typeof(CultureInlineDataTest))]
+  public async Task Error_Title_Empty(string cultureInfo)
+  {
+    var request = RequestExpenseJsonBuilder.Build();
+    request.Title = string.Empty;
+    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+    _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+    
+    var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+    result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    
+    var body = await result.Content.ReadAsStreamAsync();
+
+    var response = await JsonDocument.ParseAsync(body);
+    var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+    var expectedMessage =
+      ResourceErrorMessages.ResourceManager.GetString("TITLE_REQUIRED", new CultureInfo(cultureInfo));
+    
+    errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+
+
+  }
+
+}
